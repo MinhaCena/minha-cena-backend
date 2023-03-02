53f7256c57150a61b66f0c555f39bf9ee6b3892b
Thais <thaisminasds@gmail.com>
2023-03-01 22:59:13 -0300

    feat: add new message in constants

--- src/domain/utils/constants/messages.ts
+++ src/domain/utils/constants/messages.ts
@@ -2,12 +2,13 @@ const MESSAGE = {
     ERROR: {
       REGISTERED_USER: "User already exists!",
       EMAIL_INVALID: "Email invalid.",
-      PASSWORD_INVALID: "Password invalid."
+      PASSWORD_INVALID: "Password invalid.",
+      REGISTERED_INSTITUTION: "Institution already exists!"
     },
     SUCCESS: {
       USER_CREATED: "User created successfully!",
+      INSTITUTION_CREATED: "Institution created successfully!",
     },
   };
-  
+
   export default MESSAGE;
-  
