e1171865dbfb5e3e924e87b10fc747d6504db2a8
Maísa Lima <maisafernanda@hotmail.com>
2023-05-03 08:37:09 -0300

    refactor: fix database templating

---
+++ src/entrypoint/http/controllers/illustrator-controller.ts
@@ -0,0 +1,49 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  HttpStatus,
+  Post,
+  Param,
+ // Get,
+} from '@nestjs/common';
+import { IllustratorService } from '@domain/service/illustrator-service';
+import { Illustrator } from '@domain/entity/illustrator';
+
+@Controller('illustrator')
+export class IllustratorController {
+  constructor(private illustratorService: IllustratorService) {}
+  @Post()
+  async createIllustrator(@Body() illustrator: Illustrator) {
+    const data = await this.illustratorService.createIllustrator(illustrator);
+    if (data.name === 'error') {
+      throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: data.message,
+      });
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      message: data.message,
+    };
+  }
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    const data = await this.illustratorService.deleteIllustrator(id);
+    if (data.name === 'error') {
+      throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: data.message,
+      });
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      message: data.message,
+    };
+    // @Get()
+    // getAll() {
+    //  return this.illustratorService.getAllIllustrator();
+    // }
+  }
+}
---
+++ src/infrastructure/database/repositories/illustrator-repository.ts
@@ -0,0 +1,61 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma-service';
+import { PrismaIllustratorMapper } from '../mappers/prisma-illustrator-mapper';
+import { Illustrator } from '@domain/entity/illustrator';
+import { IllustratorClient } from '@domain/client/Illustrator-client';
+
+@Injectable()
+export class IllustratorRepository implements IllustratorClient {
+  constructor(private prismaService: PrismaService) {}
+  async create(data: Illustrator): Promise<void> {
+    const raw = PrismaIllustratorMapper.toPrisma(data);
+    try {
+      await this.prismaService.illustrator.create({
+        data: raw,
+      });
+    } catch (err: Error) {
+      if (err) {
+        return null;
+      }
+    }
+  }
+  async findByEmail(data: Illustrator): Promise<null | Illustrator> {
+    try {
+      return await this.prismaService.illustrator.findFirst({
+        where: {
+          email: data.email,
+        },
+      });
+    } catch (err: Error) {
+      if (err) {
+        return null;
+      }
+    }
+  }
+  async delete(id: number): Promise<void> {
+    try {
+      await this.prismaService.illustrator.delete({
+        where: { id: +id },
+      });
+    } catch (err: Error) {
+      if (err) {
+        return null;
+      }
+    }
+  }
+  // async getAll(data: Illustrator): Promise<Illustrator[]> {
+  //   try {
+  //     const illustrators = await this.prismaService.illustrator.findMany(data);
+  //     const newIllustrators = illustrators.map(
+  //       PrismaIllustratorMapper.toEntity,
+  //     );
+  //     return newIllustrators;
+  //   } catch (err: Error) {
+  //     if (err) {
+  //       return null;
+  //     }
+  //   }
+  //}
+}
