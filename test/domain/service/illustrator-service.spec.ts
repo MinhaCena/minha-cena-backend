ed1917800849e5ba01779611196c0f0c1248f96b
Maísa Lima <maisafernanda@hotmail.com>
2023-05-15 10:18:06 -0300

    feature/add-unit-test-illustrators

--- test/domain/service/illustrator-service.spec.ts
+++ test/domain/service/illustrator-service.spec.ts
@@ -3,13 +3,14 @@ import { IllustratorService } from '@domain/service/illustrator-service';
 import { IllustratorClient } from '@domain/client/Illustrator-client';
 import { Illustrator } from "@domain/entity/illustrator";
 import { faker } from '@faker-js/faker';
-import { faker-br } from '@faker-br';
+import * as faker_br from 'faker-br';
+
 
 const dataPerson: Illustrator = {
     id: faker.datatype.number(),
     name: faker.name.fullName(),
     email: faker.internet.email(),
-    cpfCnpj: faker.br.cpf(),
+    cpfCnpj: faker_br.br.cpf(),
     whatsapp: faker.phone.number(),
     portfolio: faker.internet.url(),
     createdAt: new Date(),
@@ -20,7 +21,7 @@ const dataCompany: Illustrator = {
     id: faker.datatype.number(),
     name: faker.name.fullName(),
     email: faker.internet.email(),
-    cpfCnpj: faker.br.cnpj(),
+    cpfCnpj: faker_br.br.cnpj(),
     whatsapp: faker.phone.number(),
     portfolio: faker.internet.url(),
     createdAt: new Date(),
@@ -41,6 +42,7 @@ const illustratorClientMock: IllustartorClientMock = {
     findById: jest.fn(),
     delete: jest.fn(),
 };
+
 describe("IllustratorService", () => {
     let illustratorService: IllustratorService;
     let illustratorClient: IllustratorClient;
@@ -60,23 +62,46 @@ describe("IllustratorService", () => {
         illustratorClient = module.get<IllustratorClient>(IllustratorClient);
     });
 
-    describe("createIllustrator", () => {
+    describe("createIllustratorPerson", () => {
         it("should return success message when illustrator does not exist", async () => {
             illustratorClientMock.findByEmail.mockResolvedValue(null);
-            illustratorClientMock.create.mockResolvedValue(data);
+            illustratorClientMock.create.mockResolvedValue(dataPerson);
 
-            const result = await illustratorService.createIllustrator(data);
+            const result = await illustratorService.createIllustrator(dataPerson);
 
             expect(result.name).toBe("success");
             expect(result.message).toBe("Illustrator created successfully!");
-            expect(illustratorClient.create).toHaveBeenCalledWith(data);
+            expect(illustratorClient.create).toHaveBeenCalledWith(dataPerson);
         });
 
         it("should return error message when illustrator already exist", async () => {
-            illustratorClientMock.findByEmail.mockResolvedValue("luiza");
-            illustratorClientMock.create.mockResolvedValue(data);
+            illustratorClientMock.findByEmail.mockResolvedValue(dataPerson);
+            illustratorClientMock.create.mockResolvedValue(dataPerson);
 
-            const result = await illustratorService.createIllustrator(data);
+            const result = await illustratorService.createIllustrator(dataPerson);
+
+            expect(result.name).toBe("error");
+            expect(result.message).toBe("Illustrator already exists!");
+        });
+    });
+
+    describe("createIllustratorCompany", () => {
+        it("should return success message when illustrator does not exist", async () => {
+            illustratorClientMock.findByEmail.mockResolvedValue(null);
+            illustratorClientMock.create.mockResolvedValue(dataCompany);
+
+            const result = await illustratorService.createIllustrator(dataCompany);
+
+            expect(result.name).toBe("success");
+            expect(result.message).toBe("Illustrator created successfully!");
+            expect(illustratorClient.create).toHaveBeenCalledWith(dataCompany);
+        });
+
+        it("should return error message when illustrator already exist", async () => {
+            illustratorClientMock.findByEmail.mockResolvedValue(dataCompany);
+            illustratorClientMock.create.mockResolvedValue(dataCompany);
+
+            const result = await illustratorService.createIllustrator(dataCompany);
 
             expect(result.name).toBe("error");
             expect(result.message).toBe("Illustrator already exists!");
@@ -85,13 +110,19 @@ describe("IllustratorService", () => {
 
     describe("deleteIllustrator", () => {
         it("should return success message when illustratorstatus is deleted", async () => {
-            illustratorClientMock.delete.mockResolvedValue(delete);
+            illustratorClientMock.delete.mockResolvedValue(dataPerson);
 
-            const result = await illustratorService.deleteIllustrator(delete);
+            const result = await illustratorService.deleteIllustrator(dataPerson.id);
+
             expect(result.name).toBe("success");
             expect(result.message).toBe("Illustrator deleted successfully!");
         });
-        it("")
-    })
-})
+        it("should return error message when illustrator is not deleted", async () => {
+            illustratorClientMock.delete.mockResolvedValue(null);
+            const result = await illustratorService.deleteIllustrator(dataPerson.id);
 
+            expect(result.name).toEqual("error");
+            expect(result.message).toEqual("Unable to delete illustrator!");
+        });
+    });
+});
